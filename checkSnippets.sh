e34f0d00dc3b7e6e5da20845f0510c38ffe35092
Tiffany Zheng <qz3@williams.edu>
2018-07-31 14:52:18 -0500

    after all struggles w javascript, problem solved w bash

--- checkSnippets.sh
+++ checkSnippets.sh
@@ -13,17 +13,15 @@ https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/02-mobile-si
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/03-mobile-robot.html
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/04-mobile-robot-description.html
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/02-falling-brick-setup.html
-https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/05-description.html
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/02-flyball-governor-simulation.html
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/03-flyball-governor-robot.html
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/04-flyball-controller.html
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/05-flyball-governor-common-control-parameters.html
 https://ihmcroboticsdocs.github.io/simulation-construction-set/docs/06-description-and-analysis.html)
 
-curl -fsS "${urls[8]}" && echo "SUCCESS" || echo "FAIL"
+#curl -fsS "${urls[8]}" && echo "SUCCESS" || echo "FAIL"
 
 for (( i = 0; i < ${#urls[@]}; ++i )); do
-	#not sure where to trigger the fail
-	curl -fsS -o /dev/null "${urls[i]}" && echo "SUCCESS" || echo "FAIL"
-	echo "Checked ${urls[i]}"
+	echo "Checking ${urls[i]}"
+	node snippetErrors.js ${urls[i]}
 done
